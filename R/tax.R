6471bf1a5512191aa4a74f7b40ffd55b9e433c30
antonioggsousa <antonioggsousa@gmail.com>
2020-09-06 18:11:43 +0100

    Added extra filtering parameters to the function 'rank_taxa()'

--- R/tax.R
+++ R/tax.R
@@ -6,30 +6,44 @@
 #' @param physeq phyloseq-class object.
 #' @param tax_rank taxonomic rank (character). One of the taxonomic ranks among
 #' the column names of 'tax_table()' of the 'physeq' object given.
-#' @param top_taxa top most abundant taxa to select by 'tax_rank' (numeric - coerced
-#' to integer). Default 'NULL'.
-#' @param type_count type of counts to perform 'rank_taxa()'. It can be absolute, i.e.,
+#' @param count_type type of counts to perform 'rank_taxa()'. It can be absolute, i.e.,
 #' 'abs', or percentage, i.e., 'perc' (character). Default is 'abs' - absolute counts.
+#' @param top_taxa top most abundant taxa to select by 'tax_rank' (numeric - coerced
+#' to integer) in the overall data set. Default 'NULL'. If 'count_type = perc', the
+#' percentage ranging from 0-100\% will be determined for the 'top_taxa' only.
+#' @param show_top top n taxa to show (numeric - coerced to integer). Default is 'NULL'.
+#' The difference to 'top_taxa' is that if 'count_type = "perc"', the percentage presented
+#' still refers to the percentage of overall data before discarding the non-top n taxa.
+#' @param group factorial variable to group, one among 'sample_variables(physeq)'.
+#' Default is 'NULL'.
+#' @param taxa_perc_cutoff percentage cutoff to remove less abundant taxa. Default is 'NULL'.
+#' @param ... parameters to be passed to the function 'filter_feature_table()'.
 #' @return It returns a list with a tibble data frame ('$data') and ggplot ('$plot')
 #' ranking the taxa picked at the taxonomic rank select at 'tax_rank'.
 #' @export
 
-rank_taxa <- function(physeq, tax_rank, top_taxa = NULL,
-                      type_count = "abs", ...) {
+rank_taxa <- function(physeq, tax_rank, count_type = "abs",
+                      top_taxa = NULL, show_top = NULL,
+                      group = NULL, taxa_perc_cutoff = NULL, ...) {
 
   # packages
   require("phyloseq")
   require("dplyr")
+  require("ggplot2")
 
   # check input
   if(class(physeq) != "phyloseq") stop(paste0(deparse(substitute(physeq)), " is not a 'phyloseq' object!"))
   if( !all( c("otu_table", "tax_table") %in% slotNames(physeq) ) ) { # check the 2 physeq slots
     stop(paste0(deparse(substitute(physeq)), " does not contain at least one of the 2 slots: 'otu_table',
-    'tax_table'!\nThe 2 slots are necessary to use the 'prevalence' function.\nAborting..."))
+    'tax_table'!\nThe 2 slots are necessary to use the 'rank_taxa' function.\nAborting..."))
   }
-  stopifnot( all(c(is.character(tax_rank), is.character(type_count))) )
-  stopifnot( tax_rank %in% rank_names(physeq) )
-  stopifnot( type_count %in% c("abs", "perc") )
+  stopifnot( all(c(is.character(tax_rank), is.character(count_type))) )
+  stopifnot( all(c(length(tax_rank) == 1, tax_rank %in% rank_names(physeq))) )
+  stopifnot( count_type %in% c("abs", "perc") )
+  stopifnot( !all(c(!is.null(show_top), !is.null(taxa_perc_cutoff))) )
+  if ( !is.null(group) ) stopifnot( all(c(length(group) == 1, group %in% sample_variables(physeq))) )
+  if ( !is.null(show_top) ) stopifnot( all(c(length(show_top)==1, is.numeric(show_top))) )
+  if ( !is.null(taxa_perc_cutoff) ) stopifnot( all(c(length(taxa_perc_cutoff)==1, is.numeric(taxa_perc_cutoff)), taxa_perc_cutoff < 100) )
   checkInteger <- (physeq@otu_table@.Data%%1==0) # to deal with double like, e.g., 1 (by default - double)
   #and not as integer (coerce 1L)
   if ( !all(checkInteger == TRUE) ) stop(paste0("otu_table(", deparse(substitute(physeq)), ") is not integer!"))
@@ -45,41 +59,150 @@ rank_taxa <- function(physeq, tax_rank, top_taxa = NULL,
     physeq_rank   <- prune_taxa(taxa = top_taxa, x = physeq_rank)
   }
 
-  # rank by abundance
+  # melt data
   physeq_rank_melted <- melt_physeq(physeq = physeq_rank)
-  physeq_rank_melted_2_plot <-
-    physeq_rank_melted %>%
-    select(.data[[tax_rank]], Abundance) %>%
-    group_by(.data[[tax_rank]]) %>%
-    summarise(Abundance = sum(Abundance)) %>%
-    filter(Abundance != 0) %>% # rm phyla without abundance
-    arrange(desc(Abundance)) %>%
-    mutate(!!tax_rank := factor(.data[[tax_rank]],
-                                levels = unique(.data[[tax_rank]])))
-  if ( type_count == "abs" ) {
-    y_lab <- "Absolute abundance"
-    abundance <- "Abundance"
-    y_max <- max(physeq_rank_melted_2_plot[, "Abundance", drop = TRUE]) * 1.1
-  } else {
+  # rank by abundance
+  if ( is.null(group) ) { # plot rank abundance for the overall data set
+    # data
     physeq_rank_melted_2_plot <-
-      physeq_rank_melted_2_plot %>%
-      mutate("Percentage" = round( Abundance / sum( Abundance ) * 100, 2) )
-    y_lab <- "Percentage (%)"
-    abundance <- "Percentage"
-    y_max <- max(physeq_rank_melted_2_plot[, "Percentage", drop = TRUE]) + 5
-  }
+      physeq_rank_melted %>%
+      select(.data[[tax_rank]], Abundance) %>%
+      group_by(.data[[tax_rank]]) %>%
+      summarise(Abundance = sum(Abundance)) %>%
+      filter(Abundance != 0) %>% # rm phyla without abundance
+      arrange(desc(Abundance)) %>%
+      ungroup(.) %>%
+      mutate_if(is.factor, as.character) %>%
+      mutate(!!tax_rank := factor(.data[[tax_rank]],
+                                  levels = unique(.data[[tax_rank]])))
 
-  # plot
-  taxa_barplot_ranked <-
-    ggplot(data = physeq_rank_melted_2_plot, aes_string(x = tax_rank,
-                                                        y = abundance)) +
-    geom_bar(stat = "identity") +
-    theme_bw() +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
-    ylab(y_lab) +
-    geom_text(aes_string(label = abundance),
-              angle = 45, vjust=0, hjust = 0, size=3) +
-    ylim(c(0, y_max))
+    if ( count_type == "abs" ) {
+      y_lab <- "Absolute abundance"
+      abundance <- "Abundance"
+      y_max <- max(physeq_rank_melted_2_plot[, "Abundance", drop = TRUE]) * 1.1
+    } else {
+      physeq_rank_melted_2_plot <-
+        physeq_rank_melted_2_plot %>%
+        mutate("Percentage" = round( Abundance / sum( Abundance ) * 100, 2) )
+      y_lab <- "Percentage (%)"
+      abundance <- "Percentage"
+      y_max <- max(physeq_rank_melted_2_plot[, "Percentage", drop = TRUE]) + 5
+    }
+
+    # show top taxa
+    if ( !is.null(show_top) ) {
+      show_top <- as.numeric(show_top)
+      list_taxa <- physeq_rank_melted_2_plot %>%
+        pull(.data[[tax_rank]]) %>%
+        levels(.)
+      physeq_rank_melted_2_plot <-
+        physeq_rank_melted_2_plot %>%
+        filter(.data[[tax_rank]] %in% list_taxa[1:show_top])
+    }
+    # filter taxa based on percentage
+    if ( !is.null(taxa_perc_cutoff) ) {
+      if ( count_type == "abs" ) {
+        physeq_rank_melted_2_plot <-
+          physeq_rank_melted_2_plot %>%
+          mutate("Percentage" = round( Abundance / sum( Abundance ) * 100, 2) ) %>%
+          filter(Percentage > taxa_perc_cutoff) %>%
+          select(-Percentage)
+        } else {
+      physeq_rank_melted_2_plot <-
+        physeq_rank_melted_2_plot %>%
+        filter(Percentage > taxa_perc_cutoff)
+        }
+      }
+
+    # plot
+    taxa_barplot_ranked <-
+      ggplot(data = physeq_rank_melted_2_plot, aes_string(x = tax_rank,
+                                                          y = abundance)) +
+      geom_bar(stat = "identity") +
+      theme_bw() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
+      ylab(y_lab) +
+      geom_text(aes_string(label = abundance),
+                angle = 45, vjust=0, hjust = 0, size=3) +
+      ylim(c(0, y_max))
+  } else { # plot rank abundance by group
+    # samples by group
+    n_samp_by_group <- physeq_rank_melted %>%
+      group_by(.data[[group]]) %>%
+      summarise("n" = n_distinct(Sample)) %>%
+      arrange(desc(n)) %>%
+      filter(!is.na(.data[[group]]))
+    facet_label <- paste0(n_samp_by_group[,group, drop = TRUE],
+                          " (n=", n_samp_by_group$n, ")")
+    names(facet_label) <- n_samp_by_group[,group, drop = TRUE]
+    #data
+    physeq_rank_melted_2_plot <-
+      physeq_rank_melted %>%
+      group_by(.data[[group]], .data[[tax_rank]]) %>%
+      summarise(Abundance = sum(Abundance)) %>%
+      filter(Abundance != 0) %>% # rm phyla without abundance
+      arrange(.data[[group]], desc(Abundance)) %>%
+      ungroup(.) %>%
+      mutate_if(is.factor, as.character) %>%
+      mutate(!!tax_rank := factor(.data[[tax_rank]],
+                                  levels = unique(.data[[tax_rank]]))) %>%
+      mutate(!!group := factor(.data[[group]],
+                               levels = unique(n_samp_by_group[,group, drop = TRUE])))
+
+    if ( count_type == "abs" ) {
+      y_lab <- "Absolute abundance"
+      abundance <- "Abundance"
+      #y_max <- max(physeq_rank_melted_2_plot[, "Abundance", drop = TRUE]) * 1.1
+    } else {
+      physeq_rank_melted_2_plot <-
+        physeq_rank_melted_2_plot %>%
+        group_by(.data[[group]]) %>%
+        mutate("Percentage" = round( Abundance / sum( Abundance ) * 100, 2) )
+      y_lab <- "Percentage (%)"
+      abundance <- "Percentage"
+      #y_max <- max(physeq_rank_melted_2_plot[, "Percentage", drop = TRUE]) + 5
+    }
+
+    # show top taxa
+    if ( !is.null(show_top) ) {
+      list_taxa <- physeq_rank_melted_2_plot %>%
+        pull(.data[[tax_rank]]) %>%
+        levels(.)
+      print(list_taxa)
+      physeq_rank_melted_2_plot <-
+        physeq_rank_melted_2_plot %>%
+        filter(.data[[tax_rank]] %in% list_taxa[1:show_top])
+    }
+    # filter taxa based on percentage
+    if ( !is.null(taxa_perc_cutoff) ) {
+      if ( count_type == "abs" ) {
+        physeq_rank_melted_2_plot <-
+          group_by(.data[[group]]) %>%
+          mutate("Percentage" = round( Abundance / sum( Abundance ) * 100, 2) ) %>%
+          filter(Percentage > taxa_perc_cutoff) %>%
+          select(-Percentage)
+        } else {
+        physeq_rank_melted_2_plot <-
+          physeq_rank_melted_2_plot %>%
+          filter(Percentage > taxa_perc_cutoff)
+        }
+      }
+
+    # plot
+    taxa_barplot_ranked <-
+      ggplot(data = physeq_rank_melted_2_plot, aes_string(x = tax_rank,
+                                                          y = abundance)) +
+      geom_bar(stat = "identity") +
+      theme_bw() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) +
+      ylab(y_lab) +
+      #geom_text(aes_string(label = abundance),
+      #          angle = 45, vjust=0, hjust = 0, size=3) +
+      facet_wrap(as.formula(paste("~", group)), scales = "free_y",
+                 labeller = labeller(.cols = facet_label),
+                 ncol = 3)
+
+  }
 
   # save output
   listOut <- list(data = physeq_rank_melted_2_plot,
@@ -115,7 +238,7 @@ filter_feature_table <- function(physeq, taxa2filter = NULL,
   if(class(physeq) != "phyloseq") stop(paste0(deparse(substitute(physeq)), " is not a 'phyloseq' object!"))
   if( !all( c("otu_table", "tax_table") %in% slotNames(physeq) ) ) { # check the 2 physeq slots
     stop(paste0(deparse(substitute(physeq)), " does not contain at least one of the 2 slots: 'otu_table',
-    'tax_table'!\nThe 2 slots are necessary to use the 'prevalence' function.\nAborting..."))
+    'tax_table'!\nThe 2 slots are necessary to use the 'filter_feature_table' function.\nAborting..."))
   }
   stopifnot( is.numeric(c(min_sample_depth, min_taxa_counts)) ) # check if input is numeric
 
